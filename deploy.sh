5747056dbf1f2d7e88e60aac08fec08c5a3e13b4
yidazh <licuh9011@gmail.com>
2017-01-21 21:51:13 +0800

    update travis config

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+mv travis_key ~/.ssh/id_rsa
+touch ~/.ssh/config
+echo -e "Host 2mih.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+rsync -vr public/* -e ssh travis@2mih.com:/home/travis/public/
