e2d540110aa8b4a5ea99321e9e43ce6ab35bfe80
yidazh <licuh9011@gmail.com>
2017-01-22 20:33:30 +0800

    add new post: deploy hexo to vps

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 chmod 600 travis_key
 mv travis_key ~/.ssh/id_rsa
-touch ~/.ssh/config
-echo -e "Host 2mih.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 rsync -vr public/* -e ssh travis@2mih.com:/home/travis/public/
